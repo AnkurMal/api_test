dec7db3457a8b56ea8afc3ac8c60de0ef1775951
Ankur Mallick <ankurmallick80@gmail.com>
2025-09-25 14:47:17 +0530

    ports

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use serde_json::{Value, json};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let port: u16 = env::var("PORT").unwrap_or("3000".to_string()).parse()?;
+
     let uri = env::var("MONGO_URI").expect("MONGO_URI environment variable must be set");
     let client = Client::with_uri_str(uri).await?;
 
@@ -21,7 +23,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .route("/posts", get(get_posts))
         .layer(Extension(shared_coll));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
     println!("Server running at http://{}", addr);
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
